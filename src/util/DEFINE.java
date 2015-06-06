9bf7e351466e34ae3c757cb5f0f6e8b0e0c908b5
SongYang <862160161@qq.com>
2015-06-06 15:35:20 +0800

    DEFINE

--- src/util/DEFINE.java
+++ src/util/DEFINE.java
@@ -1,5 +1,28 @@
 package util;
 
+/*
+ * 定义信息类
+ */
 public class DEFINE {
-
+	public static final String SYS_CONFIG="config.properties";//定义资源文件
+	public static final String SYS_PATH="PATH";//定义文件路径
+	public static final String SYS_PORT="PORT";//定义通讯端口
+	public static final String SYS_HOST="HOST";//定义通讯主机
+	public static final String SYS_DATA_USER="DATA_USER";//用户信息文件
+	public static final String SYS_DATA_MEMBER="DATA_MEMBER";//会员信息文件
+	public static final String SYS_DATA_GOODS="DATA_GOODS";//商品信息文件
+	public static final String SYS_LOGINSUCCESS="LOGINSUCCESS";//登录成功
+	public static final String SYS_USER_MANAGER="MANAGER";//仓管权限
+	public static final String SYS_USER_SALER="SALER";//收银权限
+	public static final String SYS_LOGIN="LOGIN";//请求登录
+	public static final String SYS_LOGINFAIL="LOGINFAIL";//登录失败
+	public static final String SYS_OPERATOR_SUCCESS="OPERATORSUCCESS";//操作成功
+	public static final String SYS_OPERATOR_FAIL="OPERATORFAIL";//操作失败
+	public static final String SYS_GOODS_IN="GOODSIN";//商品入库
+	public static final String SYS_GOODS_OUT="GOODSOUT";//商品出库
+	public static final String SYS_GOODS_ADD="GOODSADD";//添加商品
+	public static final String SYS_STOCK_ENOUGH="GOODSENOUGH";//库存商品不足
+	public static final String SYS_GOODS_QUERY_BYID="GOODSQUERYBYID";//按ID查询商品
+	public static final String SYS_GOODS_QUERY_ALL="GOODSQUERYALL";//查询全部商品
+	public static final String SYS_MEMBER_QUERY="MEMBERQUERY";//查询会员
 }
