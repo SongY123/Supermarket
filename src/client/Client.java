3d62771faa29887afd83bd4f0fd878e81e2b175e
Godfray Qiu <qiufengyu1024@gmail.com>
2015-06-06 17:18:40 +0800

    action listener for loginui
    ...

--- src/client/Client.java
+++ src/client/Client.java
@@ -1,5 +1,26 @@
 package client;
-
+import entity.UserExt;
+import util.InputTools;
+import util.Message;
 public class Client{ 
 	
+	public static void main(String[] args) {
+		LoginUI lui = new LoginUI();
+	}
+	
+	public static void runClient(){
+		int int_select=0;
+		
+		int_select=InputTools.getInt(null, Message.ERR_SELECT);
+		if(int_select==2){
+			return;
+		}
+		
+		if(int_select!=1){
+			System.out.println(Message.ERR_SELECT);
+			runClient();
+		}
+		UserExt.login();//ÓÃ»§µÇÂ¼
+		System.out.println(Message.OUTPUT_START+Message.OUTPUT_START+Message.OUTPUT_START);
+	}
 }
