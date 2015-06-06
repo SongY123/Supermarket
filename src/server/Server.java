c70f60c50bb14b9f41543e153fd5afb05fbf8674
SongYang <862160161@qq.com>
2015-06-06 15:30:11 +0800

    util

--- src/server/Server.java
+++ src/server/Server.java
@@ -1,10 +1,33 @@
 package server;
 
-public class Server {
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 
+import util.DEFINE;
+import util.DemoData;
+import util.Message;
+import util.Tools;
+
+public class Server {
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-			System.out.println(0);
+		System.out.println(Message.APP_NAME+"服务器启动..\n.");
+		//初始化服务器数据
+		DemoData.init();
+		ServerSocket serversocket;
+			try {
+				serversocket=new ServerSocket(Integer.parseInt(Tools.getValue(DEFINE.SYS_PORT)));
+				while(true){
+					Socket socket=serversocket.accept();
+					Thread serverThread=new ServerThread(socket);
+					serverThread.start();
+				}
+			} catch (NumberFormatException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 	}
 
 }
---
+++ src/server/ServerThread.java
@@ -0,0 +1,63 @@
+package server;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.net.Socket;
+import com.pb.entity.Datas;
+import com.pb.entity.GoodsExt;
+import com.pb.entity.MemberExt;
+import com.pb.entity.UserExt;
+import com.pb.util.DEFINE;
+
+
+public class ServerThread extends Thread{
+	private Socket socket=null;
+	
+	public ServerThread(Socket socket){
+		this.socket=socket;
+	}
+	
+	public void run(){
+			try {
+				InputStream is=socket.getInputStream();
+				ObjectInputStream ois=new ObjectInputStream(is);
+				Datas datas = (Datas) ois.readObject();
+				
+				//登录请求
+				if(DEFINE.SYS_LOGIN.equals(datas.getFlags())){
+					UserExt.loginCheck(datas, socket);	
+				}
+				
+				//入库、出库请求
+				if(datas.getFlags().equals(DEFINE.SYS_GOODS_IN) || datas.getFlags().equals(DEFINE.SYS_GOODS_OUT)){
+					GoodsExt.stockInOutCheck(datas, socket);
+				}
+				
+				//添加商品请求
+				if(datas.getFlags().equals(DEFINE.SYS_GOODS_ADD)){
+					GoodsExt.stockAddCheck(datas, socket);
+				}
+				
+				//按商品id查询请求
+				if(datas.getFlags().equals(DEFINE.SYS_GOODS_QUERY_BYID)){
+					GoodsExt.replyGoodsQueryByid(datas, socket);
+				}
+				
+				//查询全部商品
+				if(datas.getFlags().equals(DEFINE.SYS_GOODS_QUERY_ALL)){
+					GoodsExt.replyGoodsQueryAll(datas,socket);
+				}
+				
+				//查询会员请求
+				if(datas.getFlags().equals(DEFINE.SYS_MEMBER_QUERY)){
+					MemberExt.checkMemberScore(datas, socket);
+				}
+			} catch (ClassNotFoundException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+	}
+
+}
