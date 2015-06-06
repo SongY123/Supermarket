8043425b18b657b9941d1019c7410cc76b56a549
SongYang <862160161@qq.com>
2015-06-06 18:44:42 +0800

    Tools

--- src/util/Tools.java
+++ src/util/Tools.java
@@ -1,5 +1,26 @@
 package util;
 
-public class Tools {
+import java.io.IOException;
+import java.util.Properties;
 
+public class Tools {
+	private static Properties p=new Properties();
+	
+	/*
+	 * 读取配置文件 
+	 */
+	static{
+		try {
+			p.load(Tools.class.getClassLoader().getResourceAsStream(DEFINE.SYS_CONFIG));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/*
+	 * 根据key获得资源文件的value
+	 */
+	public static String getValue(String key){
+		return p.getProperty(key);
+	}
 }
