3c0045037e5d97c831f54cd31e6d458dfa029385
SongYang <862160161@qq.com>
2015-06-07 19:27:14 +0800

    database

--- src/server/Server.java
+++ src/server/Server.java
@@ -18,6 +18,9 @@ public class Server {
 	private int serverPort = 8000;//服务器端口
 	private Connection connection;
 	private Users users;//会员数据库
+	private Employee employees;//用户数据库
+	private Trade trades;//交易记录数据库
+	private Goods goods;//商品信息数据库
 	public Server(){
 		try {
 				linkDatabase();
@@ -31,7 +34,9 @@ public class Server {
 		connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/dict","root","User");
 		System.out.println("Driver loaded!");
 		users = new Users(connection);
-		
+		employees = new Employee(connection);
+		trades = new Trade(connection);
+		goods = new Goods(connection);
 		System.out.println("Database connected!");
 	  }
 	public void listen(){
@@ -51,7 +56,7 @@ public class Server {
 		//System.out.println(Message.APP_NAME+"服务器启动..\n.");
 		//初始化服务器数据
 		//DemoData.init();
-		System.out.println("Server On...\n");
+		System.out.println("Server On...");
 		new Server();
 		/*try {
 			serversocket=new ServerSocket();
